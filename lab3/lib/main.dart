0bb933800e168710748d6579c04fe33d8c1861e3
John Klint <gusjohn25@student.gu.se>
2025-04-07 18:59:28 +0200

    Copied lab2 to lab3 to continue work

---
+++ lab3/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:lab2/pages/main_view.dart';
+import 'package:lab2/ui_controller.dart';
+import 'package:provider/provider.dart';
+import 'model/recipe_database/recipe_handler.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => RecipeHandler()),
+        ChangeNotifierProvider(create: (context) => UIController()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Receptsök',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MainView(),
+    );
+  }
+}
